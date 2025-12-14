bc5bfbad4acc89cf8725561203e9158d537fdcbe
smb <130678798+sadmeansbad@users.noreply.github.com>
2025-12-14 19:06:40 +0100

    Add files via upload

--- button.c
+++ button.c
@@ -1,61 +1,47 @@
 #include "button.h"
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_events.h>
-#include <SDL3/SDL_stdinc.h>
 
-SG_Button* SG_CreateButton(SDL_Renderer *renderer) {
-    if (renderer == NULL) {
-        return NULL;
-    } 
-    SG_Button* btn;
-    btn = (SG_Button*)SDL_malloc(sizeof(SG_Button));
-    btn->renderer = renderer;
-    return btn;
-}
-
-void SG_ButtonProcessEvents(SDL_Event* event, SG_Button *btn) {
-    if (SDL_GetMouseFocus() == SDL_GetRenderWindow(btn->renderer)) { //is the button in the same window as the mouse?
-        float x, y;
-        SDL_GetMouseState(&x, &y);
-        if (x > btn->dst.x && x < btn->dst.x + btn->dst.w && y > btn->dst.y && y < btn->dst.y + btn->dst.h) {
-            if (event->type == SDL_EVENT_MOUSE_BUTTON_UP && !event->button.down) {
-                btn->state = SG_BUTTON_STATE_CLICKED;
-                return;
-            } else if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-                btn->state = SG_BUTTON_STATE_PRESSED;
-            } else {
-                btn->state = SG_BUTTON_STATE_HOVER;
+SG_ELEMENT_DATA SG_Button(SG_Context* ctx, SDL_FRect* dst) {
+    SG_ELEMENT_DATA data;
+    if (ctx->active) {
+        if (ctx->mouse_x > dst->x && 
+            ctx->mouse_x < dst->x + dst->w && 
+            ctx->mouse_y > dst->y && 
+            ctx->mouse_y < dst->y + dst->h) {
+            data.x = ctx->mouse_x - dst->x;
+            data.y = ctx->mouse_y - dst->y;
+            data.state = ctx->state;
+            if (ctx->state == SG_ELEMENT_STATE_CLICKED) {
+                ctx->state = SG_ELEMENT_STATE_HOVER;
             }
         } else {
-            btn->state = SG_BUTTON_STATE_DEFAULT;
+            data.state = SG_ELEMENT_STATE_DEFAULT;
         }
     } else {
-        btn->state = SG_BUTTON_STATE_DEFAULT;
+        data.state = SG_ELEMENT_STATE_DEFAULT;
     }
+    return data;
 }
 
-SG_BUTTON_STATE SG_RenderButton(SG_Button *btn, SDL_FRect* dst, SDL_Texture *def, SDL_Texture *hov, SDL_Texture *prsd) {
-    btn->dst.x = dst->x;
-    btn->dst.y = dst->y;
-    btn->dst.w = dst->w;
-    btn->dst.h = dst->h;
+void SG_ContextProcessEvents(SDL_Window* window, SG_Context* ctx, SDL_Event *event) {
+    ctx->active = true;
 
-    switch (btn->state) {
-        case SG_BUTTON_STATE_CLICKED:
-            SDL_RenderTexture(btn->renderer, prsd, NULL, &btn->dst);
-            btn->state = SG_BUTTON_STATE_HOVER;
-            return SG_BUTTON_STATE_CLICKED;
-            break;
-        case SG_BUTTON_STATE_HOVER:
-            SDL_RenderTexture(btn->renderer, hov, NULL, dst);
-            break;
-        case SG_BUTTON_STATE_PRESSED:
-            SDL_RenderTexture(btn->renderer, prsd, NULL, &btn->dst);
-            break;
-        default:
-            SDL_RenderTexture(btn->renderer, def, NULL, dst);    
-            break;
+    if (SDL_GetWindowFromID(event->button.windowID) != window) ctx->active = false;
+
+    ctx->mouse_x = event->button.x;
+    ctx->mouse_y = event->button.y;
+
+    if (ctx->active) {
+        if (ctx->state == SG_ELEMENT_STATE_PRESSED) {
+            if (event->type == SDL_EVENT_MOUSE_BUTTON_UP && event->button.button == SDL_BUTTON_LEFT) {
+                ctx->state = SG_ELEMENT_STATE_CLICKED;
+            }
+        } else if (ctx->state == SG_ELEMENT_STATE_HOVER && event->type == SDL_EVENT_MOUSE_BUTTON_DOWN && event->button.button == SDL_BUTTON_LEFT) {
+            ctx->state = SG_ELEMENT_STATE_PRESSED;
+        } else {
+            ctx->state = SG_ELEMENT_STATE_HOVER;
+        }
+    } else {
+        ctx->state = SG_ELEMENT_STATE_DEFAULT;
     }
-    
-    return btn->state;
 }
--- button.h
+++ button.h
@@ -1,23 +1,23 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_rect.h>
-#include <SDL3/SDL_render.h>
-#include <SDL3/SDL_stdinc.h>
 
-typedef enum SG_BUTTON_STATE {
-    SG_BUTTON_STATE_HOVER,   //Cursor is over the button but the mouse wasn't pressed
-    SG_BUTTON_STATE_PRESSED, //Mouse button is down on the button
-    SG_BUTTON_STATE_CLICKED, //Mouse button was pressed and released, mouse button down and up on the button
-    SG_BUTTON_STATE_DEFAULT  //Nothing happens to the button
-} SG_BUTTON_STATE;
+typedef enum SG_ELEMENT_STATE {
+    SG_ELEMENT_STATE_HOVER,     //Cursor is over the button but the mouse wasn't pressed
+    SG_ELEMENT_STATE_PRESSED,   //Mouse button is down on the button
+    SG_ELEMENT_STATE_CLICKED,   //Mouse button was pressed and released, mouse button down and up on the button
+    SG_ELEMENT_STATE_DEFAULT    //Nothing happens to the button
+} SG_ELEMENT_STATE;
 
-typedef struct SG_Button {
-    SDL_Renderer* renderer; 
-    SDL_FRect dst; 
-    SG_BUTTON_STATE state;
-} SG_Button;
+typedef struct SG_ELEMENT_DATA {
+    SG_ELEMENT_STATE state;     
+    float x, y;                 //where the element is being interacted with
+} SG_ELEMENT_DATA;
 
-SG_Button* SG_CreateButton(SDL_Renderer* renderer); 
+typedef struct SG_Context {
+    bool active;
+    float mouse_x, mouse_y;
+    SG_ELEMENT_STATE state;
+} SG_Context;
 
-void SG_ButtonProcessEvents(SDL_Event* event, SG_Button* btn);
+SG_ELEMENT_DATA SG_Button(SG_Context* ctx, SDL_FRect* dst);
 
-SG_BUTTON_STATE SG_RenderButton(SG_Button* btn, SDL_FRect* dst, SDL_Texture* def, SDL_Texture* hov, SDL_Texture* prsd);
+void SG_ContextProcessEvents(SDL_Window* window, SG_Context* ctx, SDL_Event* event);
